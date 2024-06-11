052cf2e503094e6128794e2c927c9a2a3c727dca
Shanthanu <shagou242@gmail.com>
2024-06-11 10:25:39 +0530

    new files added

--- webapp/models.py
+++ webapp/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+from django.db import models
+
+class Student(models.Model):
+    # Define your student model fields here
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20)
+    department = models.CharField(max_length=100)
+    # Define other fields as needed
+
+    def __str__(self):
+        return self.name
